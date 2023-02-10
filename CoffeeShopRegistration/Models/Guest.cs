002a659a26923b743371c8c9022c9e897d5a22e4
allison <allisonrjones@gmail.com>
2023-02-10 00:15:09 -0500

    Initial

---
+++ CoffeeShopRegistration/Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CoffeeShopRegistration.Models
+{
+	public class Guest
+	{   public string fname { get; set; }
+        public string lname { get; set; }
+        public string email{ get; set; }
+        public string password { get; set; }
+		public Guest()
+        { 
+            
+        }
+    }
+    
+}
+
