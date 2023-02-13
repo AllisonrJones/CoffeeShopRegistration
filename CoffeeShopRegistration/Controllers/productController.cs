fa219c2f21cf3b5daaace0a88b071a0d0bcece2d
allison <allisonrjones@gmail.com>
2023-02-13 08:54:23 -0500

    unworking draft

---
+++ CoffeeShopRegistration/Controllers/productController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class productController
+    {
+    }
+}
